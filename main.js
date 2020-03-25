40a694062ed9ef84142b1d89a76f78cb11148a98
Karl Bauer <karl@local>
2020-03-25 16:49:44 +1000

    fix

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 const Discord = require('discord.js');{}
 const bot = new Discord.Client();
 
-const token = '';
+const token = 'NjkxOTEzNTkyMDgxMjE5NjQ2.XnnMvQ._JqcjQD8f2fmfW-mbIn45lmj-HU';
 const prefix = '!';
 
 bot.on('ready', ()=>
 {
     console.log('Bot online.');
-})
+});
 
 bot.on('message', message=>{
-    let args = msg.content.substring(prefix.length).split(/ +/);
+    let args = message.content.substring(prefix.length).split(/ +/);
 switch(args[0]){
     case 'help':
         message.reply('Sorry, I cant do that!');
         break;
-    case 'about':~
+    case 'about':
         message.reply('yeah working on this one too.');
         break;
 }
-})
+});
 
 bot.on('guildMemberAdd', member=>{
     const channel = member.guild.channels.cache.find(ch => ch.name === 'welcome');
